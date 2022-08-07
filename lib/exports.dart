90cf30a0d77558ba1caa0cfdeec106fd4cd6e4ab
maxzod <maxzod66@gmail.com>
2022-08-07 17:24:30 +0200

    export typedefs

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,3 +1,4 @@
+export 'common/typedef.dart';
 export 'package:playx_core/utils/prefs.dart';
 export 'package:get/get.dart' hide Translations, ContextExtensionss;
 export 'package:flutter_readable/flutter_readable.dart';
