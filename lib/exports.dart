1a26dcdaf56c669c0a42fcfbeac4678e945988a3
maxzod <maxzod66@gmail.com>
2022-08-07 17:22:04 +0200

    depend and export equatable

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,3 +1,4 @@
 export 'package:playx_core/utils/prefs.dart';
 export 'package:get/get.dart' hide Translations, ContextExtensionss;
 export 'package:flutter_readable/flutter_readable.dart';
+export 'package:equatable/equatable.dart';
