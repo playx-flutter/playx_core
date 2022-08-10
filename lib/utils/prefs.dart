270c72716ee8ade63602df2031fa8023145be476
maxzod <maxzod66@gmail.com>
2022-08-10 22:44:14 +0200

    make prefs instance public

--- lib/utils/prefs.dart
+++ lib/utils/prefs.dart
@@ -2,51 +2,51 @@ import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class Prefs {
-  static SharedPreferences get _prefs => Get.find<SharedPreferences>();
+  static SharedPreferences get instance => Get.find<SharedPreferences>();
 
   static String? getString(
     String key,
   ) =>
-      _prefs.getString(key);
+      instance.getString(key);
 
   static Future<void> setString(
     String key,
     String value,
   ) =>
-      _prefs.setString(key, value);
+      instance.setString(key, value);
 
   static int? getInt(
     String key,
   ) =>
-      _prefs.getInt(key);
+      instance.getInt(key);
 
   static Future<void> setInt(
     String key,
     int value,
   ) =>
-      _prefs.setInt(key, value);
+      instance.setInt(key, value);
 
   static bool getBool(
     String key,
   ) =>
-      _prefs.getBool(key) ?? false;
+      instance.getBool(key) ?? false;
 
   static Future<void> setBool(
     String key,
     bool value,
   ) =>
-      _prefs.setBool(key, value);
+      instance.setBool(key, value);
 
   static Object? find(
     String key,
   ) =>
-      _prefs.get(key);
+      instance.get(key);
 
   /// clear the preferences
-  static Future<void> clear() => _prefs.clear();
+  static Future<void> clear() => instance.clear();
 
   static Future<void> remove(
     String key,
   ) =>
-      _prefs.remove(key);
+      instance.remove(key);
 }
