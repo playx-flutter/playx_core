9c7f8f291b93c84fe3369eb7d7d6d96712c4f11d
maxzod <maxzod66@gmail.com>
2022-10-03 15:35:48 +0200

    add helpers export

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,5 +1,6 @@
 export 'common/typedef.dart';
 export 'package:playx_core/utils/prefs.dart';
+export 'package:playx_core/helpers.dart';
 export 'package:get/get.dart' hide Translations, ContextExtensionss;
 export 'package:flutter_readable/flutter_readable.dart';
 export 'package:equatable/equatable.dart';
