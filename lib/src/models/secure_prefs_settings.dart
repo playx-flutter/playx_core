631e5452e45b3a5553b2a2f3b016fd9f8f90b29e
Basem Osama <engbasemosama@gmail.com>
2025-01-19 16:54:17 +0200

    feat: Added new createSecurePrefs param to `PlayxSecurePrefsSettings` which if set to `true`, the storage will be created on boot - CU-86c1jfhc3
    Signed-off-by: Basem Osama <engbasemosama@gmail.com>

---
+++ lib/src/models/secure_prefs_settings.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+///Settings for [PlayxSecurePrefsSettings] .
+class   {
+  final IOSOptions iosOptions;
+  final AndroidOptions androidOptions;
+  final LinuxOptions linuxOptions;
+  final WindowsOptions windowsOptions;
+  final WebOptions webOptions;
+  final MacOsOptions macOsOptions;
+
+  /// If [clearOnReinstall] is set to `true`, the storage is cleared on app reinstall.
+  final bool clearOnReinstall;
+
+  /// If [createSecurePrefs] is set to `true`, the storage will be created on boot.
+  final bool createSecurePrefs;
+
+  const PlayxSecurePrefsSettings({
+    this.iosOptions =
+        const IOSOptions(accessibility: KeychainAccessibility.first_unlock),
+    this.androidOptions = const AndroidOptions(
+      encryptedSharedPreferences: true,
+    ),
+    this.linuxOptions = LinuxOptions.defaultOptions,
+    this.windowsOptions = WindowsOptions.defaultOptions,
+    this.webOptions = WebOptions.defaultOptions,
+    this.macOsOptions = MacOsOptions.defaultOptions,
+    this.clearOnReinstall = true,
+    this.createSecurePrefs = true,
+  });
+}
